96d3bedac1c1bd09e5e39ec16b9725d110eba8a9
enosie <aiingel1214@gmail.com>
2023-12-03 21:15:40 -0500

    v1 audio updates

--- AED_Audio.cpp
+++ AED_Audio.cpp
@@ -3,5 +3,37 @@
 AED_Audio::AED_Audio(QObject *parent)
     : QObject{parent}
 {
+}
 
+void AED_Audio::playShockPrompts() {
+    qDebug() << "[AED Audio]: Stand Clear";
+    QThread::msleep(2000);
+    qDebug() << "[AED Audio]: Analyzing...";
+    QThread::msleep(2000);
+    qDebug() << "[AED Audio]: Shock Advised";
+
+}
+
+void AED_Audio::playNormalAudio() {
+    qDebug() << "[AED Audio]: Heart Rate is Normal";
+}
+
+void AED_Audio::responsiveAudio() {
+    qDebug() << "[AED Audio]: Check Responsivness";
+}
+
+void AED_Audio::helpCall() {
+    qDebug() << "[AED Audio]: Call for help";
+}
+
+void AED_Audio::openAirway() {
+    qDebug() << "[AED Audio]: Open Airway";
+}
+
+void AED_Audio::checkBreathing() {
+    qDebug() << "[AED Audio]: Check Breathing";
+}
+
+void AED_Audio::attachElectrode() {
+    qDebug() << "[AED Audio]: Attach Electrode Pads to patient's bare chest";
 }
--- AED_Audio.h
+++ AED_Audio.h
@@ -2,15 +2,26 @@
 #define AED_AUDIO_H
 
 #include <QObject>
+#include <QDebug>
+#include <QThread>
 
 class AED_Audio : public QObject
 {
     Q_OBJECT
 public:
     explicit AED_Audio(QObject *parent = nullptr);
+    
+    void playShockPrompts(); //called when shock is to be administered
+    void playNormalAudio(); //function is called when heart rate is normal
+    void responsiveAudio(); //emulates "check responsiveness audio"
+    void helpCall(); //call for help stage in process
+    void openAirway(); //open airway stage in process
+    void checkBreathing(); //check patient's breathing
+    void attachElectrode(); //attach electrode pads
 
-signals:
-
+private:
+    QString message;
 };
 
 #endif // AED_AUDIO_H
+
