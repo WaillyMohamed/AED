ab4d93c371b714ab5c9ccd4e4c26e97795562800
Wailly <wailly1921@hotmail.com>
2023-12-05 14:53:38 -0500

    added new cpr images to resources file

--- aed_display.cpp
+++ aed_display.cpp
@@ -24,7 +24,8 @@ AED_Display::AED_Display(QWidget *parent)
     setLabelImage(ui->a_responsiveness, ":/res/Check_Responsiveness.png", 150, 150);
     setLabelImage(ui->attach_pads, ":/res/Attach_Electrode.png", 150, 150);
     setLabelImage(ui->a_help, ":/res/Ambulance_Step.png", 150, 150);
-    setLabelImage(ui->a_cpr,  ":/res/CPR_Step.png", 150, 150);
+    setLabelImage(ui->a_cpr,  ":/res/CPR_Breath.PNG", 150, 150);
+    setLabelImage(ui->a_cpr_2,  ":/res/CPR_Compressions.PNG", 150, 150);
     setLabelImage(ui->a_standclear,  ":/res/Stand_Clear.png", 150, 150);
     setLabelImage(ui->a_heart,  ":/res/AED_Heart.png", 150, 150);
 
